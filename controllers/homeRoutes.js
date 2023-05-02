6194fc03f98b27a514c4034ccb45e50c3839cf38
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-05-02 06:58:42 -0400

    Got some of it working, but now seeing errors

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,20 +4,20 @@ const {Post, User} = require ('../models')
 router.get('/', async (req, res)=>{
     try {
         const homePage = await Post.findAll({
-            include: [{model: User}],
-            attributes: {
-                include: [
-                    [
-                        sequelize.literal(
-                            '(SELECT name From User WHERE Post_id = User.id'
-                        )
-                    ]
-                ]
-            }
+            // include: [{model: User}],
+            // attributes: {
+            //     include: [
+            //         [
+            //             sequelize.literal(
+            //                 '(SELECT name From User WHERE Post_id = User.id'
+            //             )
+            //         ]
+            //     ]
+            // }
         })
         const posts = homePage.map((post)=>
         post.get({plain: true}))
-        res.render('homepage', {posts})
+        res.render('homepage', {posts, logged_in:req.session.logged_in})
         res.status(200).json(homePage)
     } catch (err){
         res.status(500).json(err)
