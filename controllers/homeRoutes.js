050d6f0b664989d83bf982bb0d70c756926070c9
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-05-02 22:11:16 -0400

    Fixed the login and logout buttons

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,32 +3,34 @@ const sequelize = require('../config/connection')
 const {Post, User} = require ('../models')
 
 router.get('/', async (req, res)=>{
+    console.log(req.session.logged_in)
     try {
         const homePage = await Post.findAll({
-            include: [{model: User}]
+            include: [{model: User, attributes: ['id', 'name']}]
         })
         const posts = homePage.map((post)=>
         post.get({plain: true}))
         res.render('homepage', {posts, logged_in:req.session.logged_in})
-        // res.status(200).json(homePage)
     } catch (err){
         res.status(500).json(err)
     }
 })
 router.get('/dashboard', async (req, res)=>{
    try {
-    const dashBoard = await sequelize.query(`SELECT * FROM user INNER JOIN post WHERE post_id = user.id`)
-    // Post.findByPk(req.params.id,{
-    //     include:[{model: User,
-    //     attributes: ['id']}],
-        // where: {
-        //     post_id: user.id
-        // }
-    
-    // if(!req.session.logged_in) {
-    //     res.redirect('/login')
-    //     return
-    // }
+    const dashBoard = 
+    // await sequelize.query(`SELECT * FROM user INNER JOIN post WHERE post_id = user.id`)
+    await Post.findByPk(req.params.id,{
+        include:[{model: User,
+        attributes: ['id','name'],
+        where: {
+            post_id: user.id
+        }
+    }]
+})
+    if(!req.session.logged_in) {
+        res.redirect('/login')
+        return
+    }
     const myPosts = dashBoard.map((post)=>
         post.get({plain:true}))
     res.render('dashboard', {myPosts})
