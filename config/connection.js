f898dbba9f083ee5066ce1093baadf3741d8e852
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-05-02 22:49:46 -0400

    Tried some new fixes in connection js

--- config/connection.js
+++ config/connection.js
@@ -14,8 +14,21 @@ if (process.env.JAWSDB_URL) {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    }
+    },
+    {dialectOptions: {
+      ssl: {
+        require:true,
+        rejectUnauthorized: false
+      }
+    }}
   );
 }
-
+sequelize
+.authenticate()
+.then(()=>{
+  console.log('Connection has been established successfully');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err)
+})
 module.exports = sequelize;
